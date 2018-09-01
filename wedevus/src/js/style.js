b474390d01d7bf2b3ebe5468622f743997bcf001
SemBot <alexandre.daumard@gmail.com>
2018-09-01 23:14:33 +0200

    index V2
    Loader + flèche

--- wedevus/src/js/style.js
+++ wedevus/src/js/style.js
@@ -1,4 +1,23 @@
 $(window).load(function() {
- $('.loader').fadeOut('1000');
- $('.loader').hide();
+ $('.loader').fadeOut('slow', function(){
+     $('.loader').hide();
+    });
+
+});
+
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 1000;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.top-arrow').fadeIn(duration);
+        } else {
+            jQuery('.top-arrow').fadeOut(duration);
+        }
+    });
+    jQuery('.top-arrow').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
 });
