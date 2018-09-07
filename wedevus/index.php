5c4c7b4fd2e626617c980e957cd5dd90e456c9bd
SemBot <alexandre.daumard@gmail.com>
2018-09-07 23:20:15 +0200

    changes

--- wedevus/index.php
+++ wedevus/index.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-    <meta name="viewport" content"width=device-width, initial-scale=1.0,user-scalable=no">
+    <meta name="viewport" content"width=device-width, initial-scale=1.0,user-scalable=1">
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="./src/favicon.ico" type="image/x-icon">
     <link rel="icon" href="./src/favicon.ico" type="image/x-icon">
